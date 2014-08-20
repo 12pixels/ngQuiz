e8e5f616ad0693251cfd2ff276072a386a6bf3a9
12pixels <mahesh.damodhar@gmail.com>
2014-08-20 16:48:50 +0530

    Building the quiz above the scaffolding

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,8 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/login', {templateUrl: 'partials/partialLogin.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/register', {templateUrl: 'partials/partialRegister.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/quiz',{templateUrl:'partials/partialQuiz.html', controller:'quizController'});
+  $routeProvider.otherwise({redirectTo: '/quiz'});
 }]);
